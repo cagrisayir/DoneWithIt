630349bb40c7cb07d37f4e4e9198491b858c6010
Omer Cagri Sayir <omercagris@gmail.com>
2023-10-25 11:32:07 +0300

    deleted button added custom text component

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StyleSheet, View } from 'react-native';
-import AppButton from './app/components/AppButton';
+import React from 'react';
+
+import AppText from './app/components/AppText';
 
 export default function App() {
 	return (
-		<View>
-			<AppButton title='Login' onPress={() => console.log('tapped')} />
+		<View style={styles.container}>
+			<AppText>I Love React Native!</AppText>
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
