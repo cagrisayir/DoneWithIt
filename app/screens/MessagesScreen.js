885c7f0b9b46e07899aef5e09c4ccdae4f92056e
Omer Cagri Sayir <omercagris@gmail.com>
2023-10-26 14:46:24 +0300

    refreshing

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -24,6 +24,7 @@ const initialMessages = [
 
 const MessagesScreen = () => {
 	const [messages, setMessages] = useState(initialMessages);
+	const [refreshing, setRefreshing] = useState(false);
 
 	const handleDelete = message => {
 		// Delete the message from messages
@@ -46,6 +47,17 @@ const MessagesScreen = () => {
 					/>
 				)}
 				ItemSeparatorComponent={ListItemSeparator}
+				refreshing={refreshing}
+				onRefresh={() =>
+					setMessages([
+						{
+							id: 2,
+							title: 'T2',
+							description: 'D2',
+							image: require('../assets/omer.jpeg'),
+						},
+					])
+				}
 			/>
 		</Screen>
 	);
