59f82f9d5199fce9c123be29f121a33c49dee07d
Omer Cagri Sayir <omercagris@gmail.com>
2023-10-27 13:31:36 +0300

    input

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 import ListingScreen from './app/screens/ListingScreen';
+import Screen from './app/components/Screen';
+import AppTextInput from './app/components/AppTextInput';
 
 export default function App() {
-	return <ListingScreen />;
+	return (
+		<Screen>
+			<AppTextInput placeholder='username' icon='email' />
+		</Screen>
+	);
 }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,24 +1,10 @@
-import { Platform, StyleSheet, Text } from 'react-native';
+import { Text } from 'react-native';
 import React from 'react';
 
-const AppText = ({ children, style }) => {
-	return <Text style={[styles.text, style]}>{children}</Text>;
-};
+import defaultStyles from '../config/styles';
 
-const styles = StyleSheet.create({
-	text: {
-		color: 'tomato',
-		...Platform.select({
-			ios: {
-				fontSize: 20,
-				fontFamily: 'Avenir',
-			},
-			android: {
-				fontSize: 18,
-				fontFamily: 'Roboto',
-			},
-		}),
-	},
-});
+const AppText = ({ children, style }) => {
+	return <Text style={[defaultStyles.text, style]}>{children}</Text>;
+};
 
 export default AppText;
