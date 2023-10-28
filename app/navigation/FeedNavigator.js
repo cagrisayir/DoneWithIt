3d5e0e737a759264231093bae4e37fa84fb89777
Omer Cagri Sayir <omercagris@gmail.com>
2023-10-28 16:10:42 +0300

    accountnavigation

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -7,11 +7,7 @@ const Stack = createStackNavigator();
 const FeedNavigator = () => (
 	<Stack.Navigator screenOptions={{ headerShown: false }}>
 		<Stack.Screen name='Listing' component={ListingScreen} />
-		<Stack.Screen
-			name='ListingDetails'
-			component={ListingDetailsScreen}
-			options={{ headerShown: false }}
-		/>
+		<Stack.Screen name='ListingDetails' component={ListingDetailsScreen} />
 	</Stack.Navigator>
 );
 
